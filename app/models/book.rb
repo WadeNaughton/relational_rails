c26c490ab2459f085653508644eef3d0533ad6ec
Wade Naughton <wadenaughton@gmail.com>
2021-12-01 18:26:33 -0500

    validations complete

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,9 @@ class Book < ApplicationRecord
 
   belongs_to :author
 
-  validates_presence_of :name, :genre, :pages, :hardcover
+  validates_presence_of :name
+  validates_presence_of :genre
+  validates_presence_of :pages
+  validates :hardcover, inclusion: [true, false]
 
 end
--- db/migrate/20211201222235_create_book.rb
+++ db/migrate/20211201222235_create_book.rb
@@ -1,13 +1,12 @@
 class CreateBook < ActiveRecord::Migration[5.2]
   def change
     create_table :books do |t|
-      t.belongs_to :author, index: true, foreign_key: true
-      t.timestamp :created_at
-      t.timestamp :updated_at
       t.boolean :hardcover
       t.integer :pages
       t.string :name
       t.string :genre
+      t.references :author, foreign_key: true
+      t.timestamps
     end
   end
 end
