62e357ef329bb3236374f2538e1ac31366fdf6df
Kelly Anderson <kean9528@colorado.edu>
2021-12-02 15:03:32 -0700

    tests for story 5 for library

--- spec/features/libraries/guests/index_spec.rb
+++ spec/features/libraries/guests/index_spec.rb
@@ -5,13 +5,13 @@ describe 'Guests index page' do
     @library2 = Library.create!(name:"Fort Collins Public Library", city: "Fort Collins", open:true, max_capacity:323)
     @guest1 = Guest.create!(name:"Charles", age:12, new:true, library_id:@library1.id)
     @guest2 = Guest.create!(name:"Chelsea", age:23, new:true, library_id:@library2.id)
-    visit '/guests'
+    visit "/libraries/#{@library1.id}/guests"
   end
 
-  describe 'when I visit a guests index page' do
-    it 'I see each Child in the system including the Childs attributes' do
+  describe 'when I visit a libraries guests index page' do
+    it 'I see each Child that is associated with that Parent with each Childs attributes' do
       expect(page).to have_content(@guest1.name)
-      expect(page).to have_content(@guest2.name)
+      expect(page).to have_content(@guest1.age)
     end
   end
 end
