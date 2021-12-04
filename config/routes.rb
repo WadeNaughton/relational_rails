3e0725cdf85cc7bd22b534ebedaed2619141b528
Kelly Anderson <kean9528@colorado.edu>
2021-12-04 16:43:02 -0700

    reorganize routes page

--- config/routes.rb
+++ config/routes.rb
@@ -7,31 +7,24 @@ Rails.application.routes.draw do
   get '/authors/new', to:'authors#new'
   get '/authors/:id', to: 'authors#show'
 
-
-
-
   #books
   get '/books', to: 'books#index'
   post '/books', to: 'books#create'
   get '/books/new', to:'books#new'
-  get '/guests/:id', to: 'guests#show'
-  get 'libraries/:library_id/guests', to: 'library_guests#index'
-
-  
   get '/books/:id', to: 'books#show'
 
-
+  #libraries
+  get 'libraries/:library_id/guests', to: 'library_guests#index'
   get '/libraries', to: 'libraries#index'
-  get '/guests', to: 'guests#index'
-
-  get '/authors/:id', to: 'authors#show'
   get '/libraries/new', to: 'libraries#new'
   post '/libraries', to: 'libraries#create'
+  get '/libraries/:id', to: 'libraries#show'
+
+  #guests
+  get '/guests', to: 'guests#index'
   get '/guests/new', to: 'guests#new'
   post '/guests', to: 'guests#create'
-
-  get '/libraries/:id', to: 'libraries#show'
   get '/authors/:author_id/books', to: 'author_books#index'
-
+  get '/guests/:id', to: 'guests#show'
 
 end
